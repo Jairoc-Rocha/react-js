f716168818abaf0ee2c9e044123c5868fe546f68
Jairo Rocha <jairoc.rocha10@gmail.com>
2023-08-24 15:35:05 -0300

    Iniciando o curso de react-js

---
+++ Modulo-I/react-hello/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="bg-gray-100 mx-auto p-4">
+        <h1 className="text-center font-semibold text-xl">react-hello</h1>
+      </div>
+    </header>
+  );
+}
