70a68b220f40873f983c685eca3e8f0edaba455b
Jairo Rocha <jairoc.rocha10@gmail.com>
2023-08-30 14:36:47 -0300

    Segunda parte do projeto react-countries

--- Modulo-I/react-countries/src/pages/ReactCountriesPage.jsx
+++ Modulo-I/react-countries/src/pages/ReactCountriesPage.jsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import Main from "../components/Main";
+import TextInput from "../components/TextInput";
+import { allCountries } from "../data/countries";
 
 export default function ReactCountriesPage() {
+  const [countryFilter, setCountryFilter] = useState("Argentina");
+
+  function handleCountryFilterChange(newCountryFilter) {
+    setCountryFilter(newCountryFilter);
+  }
+
+  const countryFilteredLowercase = countryFilter.toLocaleLowerCase();
+
+  const filteredCountries =
+    countryFilteredLowercase.length >= 3
+      ? allCountries.filter(({ nameLowerCase }) => {
+          return nameLowerCase.includes(countryFilteredLowercase);
+        })
+      : allCountries;
+
+  console.log(filteredCountries);
+
   return (
     <div>
       <Header>react-countries</Header>
-      <Main>conteúdo</Main>
+      <Main>
+        <TextInput
+          id="inputCountryFilter"
+          labelDescription="Informe o nome do país (pelo menos 3 caracteres)"
+          inputValue={countryFilter}
+          onInputChange={handleCountryFilterChange}
+        />
+      </Main>
     </div>
   );
 }
